3e41c121c5228408e4611bab5c07d70c78d0e4f5
Alice131419 <manuscarlet@yahoo.com>
2024-11-26 10:56:37 +0200

    updated JavaScript, CSS and HTML

--- src/index.js
+++ src/index.js
@@ -13,9 +13,13 @@ function generatePoem(event) {
   let instructionsInput = document.querySelector("#user-instructions");
   let apiKey = "3be4c0t6ebabc0db7oebb0b39afcd366";
   let context =
-    "You are a romantic Poem expert and love to write short poems. You`re mission is to generate a 4 line poem in basic HTML without mention HTML. Make sure to follow the user instructions.";
-  let prompt = `User instructions: Generate an English poem about ${instructionsInput.value}`;
+    "You are a romantic Poem expert and love to write short poems. You`re mission is to generate a 4 line poem in basic HTML, NO mention HTML. Make sure to follow the user instructions.";
+  let prompt = `User instructions: Generate an Italian poem about ${instructionsInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="blink">Generating an Italian poem about ${instructionsInput.value}</div>`;
 
   axios.get(apiUrl).then(displayPoem);
 }
